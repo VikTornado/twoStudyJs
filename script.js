13b019c2cfe96592e0aa2072e05f6083b8232000
React <3809164@gmail.com>
2022-09-01 15:04:19 +0300

    2

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ let screens = prompt(
 );
 let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
 let adaptive = confirm("Нужен ли адаптив на сайте?");
+
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
+
 let rollback = 10;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+
+if (fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice > 15000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+} else console.log("Что то пошло не так");
 
 console.log("Мой проект называется" + " = " + title);
 console.log("Мне нужен" + " = " + screens);
@@ -22,26 +32,3 @@ console.log("это будет стоить" + "=" + "$" + servicePrice2);
 console.log(fullPrice);
 console.log("Полная цена " + "$" + fullPrice);
 console.log("Цена со скидкой" + " " + Math.floor(servicePercentPrice));
-
-if (fullPrice > 30000) {
-  console.log("Даем скидку в 10%");
-} else if (fullPrice > 15000) {
-  console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000) {
-  console.log("Скидка не предусмотрена");
-} else console.log("Что то пошло не так");
-
-// switch (fullPrice) {
-//   case fullPrice > 30000:
-//     console.log("Даем скидку в 10%");
-//     break;
-//   case fullPrice > 15000:
-//     console.log("Даем скидку в 5%");
-//     break;
-//   case fullPrice < 15000:
-//     console.log("Скидка не предусмотрена");
-//     break;
-//   default:
-//     console.log("Что то пошло не так");
-//     break;
-// }
